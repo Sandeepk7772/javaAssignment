f3ab364afa7c0327274025a75c576e0f9c9a6421
Sandeep <sandeepk7772@gmail.com>
2020-08-22 03:54:16 -0700

    added solution for add colors in ArrayList

---
+++ src/AddColorsInArrayList.java
@@ -0,0 +1,20 @@
+package javaAssignment;
+
+import java.util.*;
+
+public class AddColorsInArrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ ArrayList<String> colors = new ArrayList<String>();
+ colors.add("Red");
+ colors.add("Green");
+ colors.add("Orange");
+ colors.add("White");
+ colors.add("Black");
+  
+  
+  System.out.println(colors);
+	}
+
+}
