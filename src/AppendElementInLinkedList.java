4732b5d8c44149c1421a3d1a80e472c04174c9d2
Sandeep <sandeepk7772@gmail.com>
2020-08-23 01:58:51 -0700

    added Solution For Append Element In LinkedList

---
+++ src/AppendElementInLinkedList.java
@@ -0,0 +1,25 @@
+package javaAssignment;
+
+import java.util.*;
+
+public class AppendElementInLinkedList {
+
+	public static void main(String[] args) {
+		
+		     LinkedList<String> inputList = new LinkedList<String>();
+		     
+		     inputList.add("Red");
+		     inputList.add("Green");
+		     inputList.add("Black");
+		     inputList.add("White");
+		     inputList.add("Pink");
+		     inputList.add("Yellow");
+			
+		  
+		   System.out.println("The linked list:" +inputList);
+		   
+		
+
+	}
+
+}
