33d4cce5dcc416ae4e1c6ce0dd33ba97d062dfb1
Sandeep <sandeepk7772@gmail.com>
2020-08-23 01:11:42 -0700

    added Solution For Append Element In HashSet

---
+++ src/AppendElementInHashSet.java
@@ -0,0 +1,23 @@
+package javaAssignment;
+import java.util.*;
+
+
+public class AppendElementInHashSet {
+
+	public static void main(String[] args) {
+		 HashSet<String> inputHshSet = new HashSet<String>();
+		   
+		 inputHshSet.add("Red");
+		 inputHshSet.add("Green");
+		 inputHshSet.add("Black");
+		 inputHshSet.add("White");
+		 inputHshSet.add("Pink");
+		 inputHshSet.add("Yellow");
+
+		  
+		  System.out.print("The Hash Set: " +inputHshSet);
+		   }
+		
+
+	}
+
