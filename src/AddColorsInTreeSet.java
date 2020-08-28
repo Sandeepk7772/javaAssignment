069fea60b2ba37f8fac7519786bb1fc2372ec8dc
Sandeep <sandeepk7772@gmail.com>
2020-08-28 08:20:07 -0700

    added Solution For add Colors In Tree Set

---
+++ src/AddColorsInTreeSet.java
@@ -0,0 +1,19 @@
+
+    import java.util.*;
+
+    public class AddColorsInTreeSet {
+        public static void main(String[] args) {
+            TreeSet<String> inputColors = new TreeSet<String>();
+
+            inputColors.add("Red");
+            inputColors.add("Green");
+            inputColors.add("Orange");
+            inputColors.add("White");
+            inputColors.add("Black");
+
+            System.out.print("Tree set: ");
+
+            System.out.println(inputColors);
+        }
+    }
+
